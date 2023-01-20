d700f19570d2e66ccb91f0c07e3c4fb30ce8976e
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-20 09:26:33 -0500

    C-14 ADD Redux AuthModalSlice

--- client/src/redux/features/store.jsx
+++ client/src/redux/features/store.jsx
@@ -1,17 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authModalSlice from "./features/authModalSlice";
-import globalLoadingSlice from "./features/globalLoadingSlice";
-import themeModeSlice from "./features/themeModeSlice";
-import userSlice from "./features/userSlice";
+import appSateSlice from "./features/appSateSlice";
 import appSateSlice from "./features/appSateSlice";
 
 const store = configureStore({
   reducer: {
-    user: userSlice,
-    themeMode: themeModeSlice,
+    appState: appSateSlice,
     authModal: authModalSlice,
-    globalLoading: globalLoadingSlice,
-    appState: appSateSlice
+
   }
 });
 
