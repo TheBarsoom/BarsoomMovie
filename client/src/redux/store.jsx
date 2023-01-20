c130b69917e19db57c4af4558cdfe612a78d474c
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-20 09:52:18 -0500

    C-16 ADD Redux GlobalLoadingSlice

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authModalSlice from "./features/authModalSlice";
 import appSateSlice from "./features/appSateSlice";
 import themeModeSlice from "./features/themeModeSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
 
 const store = configureStore({
   reducer: {
     appState: appSateSlice,
     authModal: authModalSlice,
     themeMode: themeModeSlice,
+    globalLoading: globalLoadingSlice,
+
 
   }
 });
