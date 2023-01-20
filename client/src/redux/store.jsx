ff741000888af42225f5e75093f44d3a4f921556
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-20 09:32:21 -0500

    C-15 ADD Redux ThemeModeSlice

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authModalSlice from "./features/authModalSlice";
 import appSateSlice from "./features/appSateSlice";
-import appSateSlice from "./features/appSateSlice";
+import themeModeSlice from "./features/themeModeSlice";
 
 const store = configureStore({
   reducer: {
     appState: appSateSlice,
     authModal: authModalSlice,
+    themeMode: themeModeSlice,
 
   }
 });
